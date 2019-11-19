d2da745a9d8c56c0cf1fd1bb69cb86db50111476
Hamzah Zia <hamzah.uzair@hotmail.com>
2019-11-19 18:39:48 -0500

    reworked imagecomparer

--- ImageComparer/imageComparer.py
+++ ImageComparer/imageComparer.py
@@ -1,10 +1,11 @@
 from skimage import metrics
 import numpy as np
-#import matplotlib.pyplot as plt
 import cv2
 import sys
 import os
 import re
+import time
+# import matplotlib.pyplot as plt
 
 def mse(imageA, imageB):
 	err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
@@ -41,50 +42,81 @@ def compareHist(imageA, imageB):
     
     # Calculate the histogram and normalize it
     hist_imga = cv2.calcHist([imga_hsv], [0,1], None, [180,256], [0,180,0,256])
-    cv2.normalize(hist_imga, hist_imga, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX);
+    cv2.normalize(hist_imga, hist_imga, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX)
     hist_imgb = cv2.calcHist([imgb_hsv], [0,1], None, [180,256], [0,180,0,256])
-    cv2.normalize(hist_imgb, hist_imgb, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX);
+    cv2.normalize(hist_imgb, hist_imgb, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX)
     
     # find the metric value
     metric_val = cv2.compareHist(hist_imga, hist_imgb, cv2.HISTCMP_CORREL)
     return metric_val
 
-def main(dir, mins):
+def main(dir, minr):
+    # Output directory for frames to keep
     outpath = dir + "../out/"
     # Need to sort images in order by frame number
-    images = sorted(os.listdir(dir), key=(lambda p: int(re.search("_[0-9]*", p).group()[1:])))
+    og_images = sorted(os.listdir(dir), key=(lambda p: int(re.search("_[0-9]+", p).group()[1:])))
+    print(f"Number of frames: {len(og_images)}")
+    ''' 
+     compareNeighbours: compares each image in images to its immediate successor in the array
+                        if they are too similar it will discard the successor
+                        else it will add the successor to the returned array 
+    '''
+    def compareNeighbours(images):
+        ret = []
+        for ind, image in enumerate(images):
+            if ind == len(images) - 1:
+                break
+
+            ima = None
+            if ind == 0:
+                path1 = dir + image
+                ima = cv2.imread(path1, 1)
+                if ima is None:
+                    raise Exception(f"{path1} not found.")
+                # first frame is unique and should be added to return array
+                ret.append(image)
+            else:
+                ima = imb
+
+            # Check if next frame is similar to current frame    
+            path2 = dir + images[ind + 1]
+            imb = cv2.imread(path2, 1)
+            if imb is None:
+                raise Exception(f"{path2} not found.")
+            
+            r = compareHist(ima, imb)
+            #print(f"'{image}' and '{images[ind + 1]}': \t r = {r}")
+
+            # If not similar add unique frame to return array
+            if float(r) < minr:
+                ret.append(images[ind + 1])
+        return ret
     
-    for ind, image in enumerate(images):
-        # print(images[ind])
-        ima = None
-        if ind == 0:
-            path1 = dir + image
-            ima = cv2.imread(path1, 1)
-            if ima is None:
-                raise Exception(f"{path1} not found.")
-            cv2.imwrite(outpath + image, ima)
+    discarded_frames = 0
+    iterations = 0
+    while True:
+        iterations += 1
+        filtered_images = compareNeighbours(og_images)
+        '''
+        if length of output array is equal to input array no images were filtered
+            thus all frames are *unique*
+        '''
+        if len(filtered_images) == len(og_images):
+            print(f"Discarded frames: {discarded_frames}")
+            print(f"Iterations: {iterations}")
+            for im_name in filtered_images:
+                image = cv2.imread(dir + im_name)
+                cv2.imwrite(outpath + im_name, image)
+            return
         else:
-            ima = imb
-
-        # Write unique first frame to output directory
-        
-        if ind == len(images) - 1:
-            break
-
-        # Check if next frame is similar to current frame    
-        path2 = dir + images[ind + 1]
-        imb = cv2.imread(path2, 1)
-        if imb is None:
-            raise Exception(f"{path2} not found.")
-        
-        s = compareHist(ima, imb)
-        print(f"'{image}' and '{images[ind + 1]}': \t s = {s}")
-        # If not similar write unique frame to output directory
-        if float(s) < mins:
-            cv2.imwrite(outpath + images[ind + 1], imb)
+            discarded_frames += len(og_images) - len(filtered_images)
+            og_images = filtered_images
+    
         
 if __name__ == "__main__":
+    start = time.time()
     if len(sys.argv) != 3:
-        print("Usage: python imageComparer.py [image-directory] [min simmiliarity required]")
+        print("Usage: python imageComparer.py [image-directory] [min similarity required]")
     else:
         main(sys.argv[1], float(sys.argv[2]))
+    print(f"Execution time: {time.time() - start} seconds")
